6c0a406c1fdd61291b6c8e3bfdecfd42109938ae
Nikolaos <lunaticnick@hotmail.com>
2018-01-29 14:29:26 +0000

    Tidy up test descriptions

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -1,26 +1,30 @@
 describe("Transaction", function() {
 
-  it("Transaction should be created when a credit is made to the account", function() {
-    transaction = new Transaction();
-    expect(transaction.recordCredit(5) instanceof Array).toEqual(true);
+  describe("Deposit Transaction", function() {
+
+    it("Transaction should be created when a credit is made to the account", function() {
+      transaction = new Transaction();
+      expect(transaction.recordCredit(5) instanceof Array).toEqual(true);
+    });
+
+    it("Transaction should record when a credit is made to the account", function() {
+      transaction = new Transaction();
+      expect(transaction.recordCredit(5)).toEqual(["29/01/2018", 5, "    "]);
+    });
+
   });
 
-  it("Transaction should record when a credit is made to the account", function() {
-    transaction = new Transaction();
-    expect(transaction.recordCredit(5)).toEqual(["29/01/2018", 5, "    "]);
+  describe("Withdrawal Transaction", function() {
+
+    it("Transaction should be created when a debit is made to the account", function() {
+      transaction = new Transaction();
+      expect(transaction.recordDebit(5) instanceof Array).toEqual(true);
+    });
+
+    it("Transaction should record when a debit is made to the account", function() {
+      transaction = new Transaction();
+      expect(transaction.recordDebit(5)).toEqual(["29/01/2018", "    ", 5]);
+    });
+
   });
-
-
-
-  it("Transaction should be created when a debit is made to the account", function() {
-    transaction = new Transaction();
-    expect(transaction.recordDebit(5) instanceof Array).toEqual(true);
-  });
-
-  it("Transaction should record when a debit is made to the account", function() {
-    transaction = new Transaction();
-    expect(transaction.recordDebit(5)).toEqual(["29/01/2018", "    ", 5]);
-  });
-
-
 });
--- src/bank.js
+++ src/bank.js
@@ -2,20 +2,21 @@ const MINIMUM_BALANCE = 0
 const HEADLINE =  'date || credit || debit || balance'
 
 function Account() {
- this.balance = MINIMUM_BALANCE
+ this._balance = MINIMUM_BALANCE;
+ this._transactions = [];
 }
 
 Account.prototype.showBalance = function() {
-  return this.balance;
+  return this._balance;
 };
 
 Account.prototype.deposit = function(amount) {
-  this.balance += amount;
+  this._balance += amount;
 };
 
 Account.prototype.withdraw = function(amount) {
-  if(this.balance > MINIMUM_BALANCE) {
-    this.balance -= amount;
+  if(this._balance > MINIMUM_BALANCE) {
+    this._balance -= amount;
   } else {
     throw Error ("Not enough funds in account");
   };
